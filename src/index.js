ac2b0acbcf12bcc57f9e9ad730b269300ed7b2fc
Co-Ji <sungji226@gmail.com>
2022-06-10 00:10:28 +0900

    새로운 배포용 레포지토리

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './shared/App';
+import reportWebVitals from './reportWebVitals';
+import './shared/language/i18n';
+
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+reportWebVitals();
